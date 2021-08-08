4d710ec9a990156c6fa9d67944ac3f361dce15d5
Ender Nuvmen <theendercore@gmail.com>
2021-08-08 21:41:59 +0300

    1.17.1 update

--- src/main/java/ender/iron_coated_trapdoor/ICTD.java
+++ src/main/java/ender/iron_coated_trapdoor/ICTD.java
@@ -1,12 +1,13 @@
 package ender.iron_coated_trapdoor;
 
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Material;
 import net.minecraft.block.TrapdoorBlock;
 import net.minecraft.sound.BlockSoundGroup;
 
 public class ICTD extends TrapdoorBlock {
     protected ICTD() {
-        super(FabricBlockSettings.of(Material.STONE).nonOpaque().strength(5.0F).sounds(BlockSoundGroup.METAL));
+        super(FabricBlockSettings.of(Material.STONE).nonOpaque().strength(5.0F).sounds(BlockSoundGroup.METAL).requiresTool().breakByTool(FabricToolTags.PICKAXES, 1));
     }
 }
